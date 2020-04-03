b1eb63ee90b04d8074763d76d1004328f48f6dba
Phong Nguyen <nguyendoanphong.it@gmail.com>
2020-04-03 18:24:38 +0700

    Adding Vue (#34)

---
+++ src/ClassifiedAds.UIs/vuejs/src/main.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import axios from 'axios'
+import Vuelidate from 'vuelidate'
+import { BootstrapVue } from 'bootstrap-vue'
+
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.config.productionTip = false;
+
+Vue.filter("appendVersion", function (value) {
+  return value + " " + Vue.version;
+});
+Vue.filter("appendCurrentDateTime", function (value) {
+  return value + " " + new Date();
+});
+Vue.filter("uppercase", function (value) {
+  return value.toUpperCase();
+});
+
+Vue.use(Vuelidate)
+Vue.use(BootstrapVue)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
